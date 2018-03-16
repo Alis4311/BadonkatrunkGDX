72f318c772cb297b875487e74ac71883fd7a23ec
Christoffer Book <Alis4311@gmail.com>
2018-03-16 10:36:10 +0100

    initial commit

---
+++ ios/src/com/chris/badonkatrunk/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.chris.badonkatrunk;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.chris.badonkatrunk.Badonkatrunk;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new Badonkatrunk(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
