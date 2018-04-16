b802b160bbbd1e89351568b1639620130a6420e8
DanielRosdahl <Daniel.Rosdahl3@gmail.com>
2018-04-16 03:41:36 +0200

    MenuScreen med ImageButton(libGDX) knappar ifrån MenuButton klassen

--- core/src/Screens/LevelsScreen.java
+++ core/src/Screens/LevelsScreen.java
@@ -110,7 +110,7 @@ public class LevelsScreen implements Screen{
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glBlendColor(255,0,0,1);
+        Gdx.gl.glClearColor(0,1,1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         stage.draw();
