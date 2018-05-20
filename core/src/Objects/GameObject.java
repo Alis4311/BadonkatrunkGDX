bc01025bbdb2c7f66970bd91137bc8a33fc3e4e1
Christoffer Book <Alis4311@gmail.com>
2018-05-20 12:45:24 +0200

    Fixat skit igen

--- core/src/Objects/GameObject.java
+++ core/src/Objects/GameObject.java
@@ -1,14 +1,12 @@
 package Objects;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
+
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public abstract class GameObject {
     protected Sprite sprite;
-    private int id;
+
 
     GameObject(Sprite sprite){
         this.sprite = sprite;
@@ -38,56 +36,5 @@ public abstract class GameObject {
 
         sprite.draw(batch);
     }
-
-    public int getId(){
-        System.out.println(this.id);
-        return this.id;
-
-    }
-
-    private Sprite getSpriteForID(int id){
-        Texture texture = new Texture(Gdx.files.internal("error.png"));
-        switch(id){
-            case 0: texture = new Texture(Gdx.files.internal("cobble.png"));
-                break;
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            case 5:
-                break;
-            case 6:
-                break;
-            case 7:
-                break;
-            case 8:
-                break;
-            case 9: texture = new Texture(Gdx.files.internal("finish.png"));
-                break;
-            case 10: texture =  new Texture(Gdx.files.internal("house.png"));
-                break;
-            case 11:
-                break;
-            case 12:
-                break;
-            case 13:
-                break;
-            case 14:
-                break;
-            case 15:
-                break;
-            case 16:
-                break;
-            case 17:
-                break;
-        }
-
-        return new Sprite(texture);
-    }
-
 
 }
